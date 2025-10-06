ca585fe335aeb4f1b8265606a751602cdc734d63
Rob Svirskas <robsvi@gmail.com>
2025-10-06 10:40:29 -0400

    Increased OpenAlex sleep time

--- sync/bin/update_dois.py
+++ sync/bin/update_dois.py
@@ -7,7 +7,7 @@
            to DIS MongoDB.
 """
 
-__version__ = '17.0.0'
+__version__ = '17.1.0'
 
 import argparse
 import collections
@@ -831,7 +831,7 @@ def add_tags(persist):
     '''
     coll = DB['dis'].orcid
     for key, val in tqdm(persist.items(), desc='Add jrc_author and jrc_tag'):
-        sleep(0.15)
+        sleep(0.2)
         try:
             rec = DB['dis'].dois.find_one({"doi": key})
         except Exception as err:
@@ -924,7 +924,7 @@ def add_first_last_authors(rec):
     if (not first) and ('jrc_last_author' not in rec):
         return
     first = []
-    sleep(0.15)
+    sleep(0.2)
     try:
         det = DL.get_author_details(rec, DB['dis']['orcid'])
     except Exception as err:
@@ -945,7 +945,7 @@ def add_openalex(rec):
         Returns:
           None
     '''
-    sleep(0.15)
+    sleep(0.2)
     try:
         data = DL.get_doi_record(rec['doi'], source='openalex')
     except Exception:
